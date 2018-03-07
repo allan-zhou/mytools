183d221b0eb6104da5edbed5458ee86dbe5bc83b
loopzhou <izhoujialiang@qq.com>
2018-03-07 18:40:59 +0800

    update mysqlstring-format

--- mysqlstring-format/index.js
+++ mysqlstring-format/index.js
@@ -12,18 +12,41 @@ const rl = readline.createInterface({
 rl.on('line', (line) => {
     line = line.trim();
 
-    //1-字段名称 2-字段类型 3-默认值 4-字段描述
-    // parseFieldName(line);
+    if (!isFieldsLine(line)) {
+        parseTableName(line);
+    } else {
+        //1-字段名称 2-字段类型 3-默认值 4-字段描述
+        // parseFieldName(line);
 
-    // parseFieldType(line);
+        // parseFieldType(line);
 
-    parseFieldComment(line);
+        parseFieldComment(line);
+    }
 });
 
 rl.on('close', () => {
     // console.log('\n\n=====end=====\n\n');
 });
 
+
+function isFieldsLine(lineString) {
+    if (lineString.trim().length == 0) return false;
+    if (lineString.indexOf('CREATE TABLE') > -1) return false;
+    if (lineString.indexOf('ENGINE=InnoDB') > -1) return false;
+    if (lineString.indexOf('PRIMARY KEY') > -1) return false;
+    if (lineString.indexOf('KEY') > -1) return false;
+
+    return true;
+}
+
+function parseTableName(lineString) {
+    if (lineString.indexOf('CREATE TABLE') > -1) {
+        var startIndex = lineString.indexOf('`') + 1;
+        var endIndex = lineString.lastIndexOf('`');
+        var tableName = lineString.substring(startIndex, endIndex);
+        console.log(`==========${tableName}==========`);
+    }
+}
 
 function parseFieldName(lineString) {
     var fieldName = lineString.substring(1, lineString.indexOf('`', 1))
@@ -35,10 +58,10 @@ function parseFieldType(lineString) {
     var endIndex = lineString.indexOf('NOT NULL');
     if (endIndex < 0) {
         endIndex = lineString.indexOf('DEFAULT');
-        if(endIndex < 0) {
+        if (endIndex < 0) {
             endIndex = lineString.indexOf('COMMENT');
         }
-    } 
+    }
     var fieldType = lineString.substring(startIndex, endIndex)
     console.log(fieldType.trim());
 }
